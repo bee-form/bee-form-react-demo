0716ded04966097b7468dd69481a0903c3421b84
Le Anh Quan <quanla2003@gmail.com>
2018-07-06 15:02:47 +0700

    Config shortcuts

--- src/demo-react/examples/example-date-time/example-date-time-config.js
+++ src/demo-react/examples/example-date-time/example-date-time-config.js
@@ -7,5 +7,7 @@ module.exports = {
     description: `This time we have 2 input fields but mapped to a single data entry`,
     codes: [
         require("!raw-loader!./example-date-time"),
+        {name: "tunnel-date-string.js", code: require("!raw-loader!./tunnel-date-string")},
+        {name: "tunnel-time-string.js", code: require("!raw-loader!./tunnel-time-string")},
     ],
 };
